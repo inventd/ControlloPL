a7d7a98c6e5645130af540df96a20b9a8e48fe12
Diego <diego.venturini@panificiobraga.it>
2022-06-13 21:11:36 +0200

    Ottimizzazione codice

--- ControlloPL.ino
+++ ControlloPL.ino
@@ -1,11 +1,12 @@
+#include <Arduino.h>
 #include <Servo.h>
 
 #define NUMERO_DI_74HC165 2
 #define DATA_WIDTH NUMERO_DI_74HC165 * 8
 
+#define NUMERO_MAX_PL 3 // Numero max di passaggi a livello gestibili
+
 #undef INPUT_SERIAL
-#undef SIMULATORE
-#undef SERVO360
 
 /*
  * Defiinizione pin_out
@@ -33,7 +34,7 @@
 /*
  * Macro
  */
-#define SENS_PL(N, I) byte(I >> (4 * (N - 1)) & 0x0F)
+#define SENS_PL(N, I) byte(I >> (4 * N) & 0x0F)
 #define GATE_A(I) byte(I & 0x03)
 #define GATE_B(I) byte((I >> 2) & 0x03)
 #define STATO_A(I) byte(I & 0x0F)
@@ -49,43 +50,48 @@
 #define GATE_USCITA 0x03   // 00000010
 
 // Definizione ingressi
-#define SENS_NULL B0000
-#define SENS_INGRESSO B0001
-#define SENS_USCITA B0010
-#define SENS_FULL B0011
+#define SENS_NULL 0x00     // 0000
+#define SENS_INGRESSO 0x01 // 0001
+#define SENS_USCITA 0x02   // 0010
+#define SENS_FULL 0x03     // 0011
 
-// stati sbarra
-#define SBARRA_ABBASSATA 0
-#define SBARRA_ALZATA 1
+typedef struct {
+  byte statoPL;
+  int contaAssi;
+  Servo servoPL;
+  uint8_t semaforo;
+} PassaggioLivello;
 
 unsigned long letturaSensori;
 unsigned long oldLetturaSensori;
-byte statoPL1;
-byte statoSbarraPL1 = SBARRA_ALZATA;
-int contaAssiPL1 = 0;
-byte statoPL2;
-byte statoSbarraPL2 = SBARRA_ALZATA;
-int contaAssiPL2 = 0;
-byte statoPL3;
-byte statoSbarraPL3 = SBARRA_ALZATA;
-int contaAssiPL3 = 0;
 
-Servo servoPL1;
-Servo servoPL2;
-Servo servoPL3;
+PassaggioLivello passaggioLivello[NUMERO_MAX_PL];
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  servoPL1.attach(SERVO_PL1);
-  pinMode(SEMAFORO_PL1, OUTPUT);
-  servoPL2.attach(SERVO_PL2);
-  pinMode(SEMAFORO_PL2, OUTPUT);
-  servoPL3.attach(SERVO_PL3);
-  pinMode(SEMAFORO_PL3, OUTPUT);
+  passaggioLivello[0].contaAssi = 0;
+  passaggioLivello[0].statoPL = 0;
+  passaggioLivello[0].servoPL.attach(SERVO_PL1);
+  passaggioLivello[0].semaforo = SEMAFORO_PL1;
 
+  passaggioLivello[1].contaAssi = 0;
+  passaggioLivello[1].statoPL = 0;
+  passaggioLivello[1].servoPL.attach(SERVO_PL2);
+  passaggioLivello[1].semaforo = SEMAFORO_PL2;
+
+  passaggioLivello[2].contaAssi = 0;
+  passaggioLivello[2].statoPL = 0;
+  passaggioLivello[2].servoPL.attach(SERVO_PL3);
+  passaggioLivello[2].semaforo = SEMAFORO_PL3;
+
+  for (size_t i = 0; i < NUMERO_MAX_PL; i++)
+  {
+    pinMode(passaggioLivello[i].semaforo, OUTPUT);
+  }
+  
   // Configurazione linee per gestione shift register 74HC165
 #ifdef INPUT_SERIAL
   pinMode(LOAD_PIN, OUTPUT);
@@ -108,12 +114,7 @@ void setup()
 
 void loop()
 {
-#ifdef SIMULATORE
-  letturaSensori = simulaIngressi(oldLetturaSensori);
-#else
   letturaSensori = leggiSensoriPL();
-  // Serial.print("x");
-#endif
 
   if (letturaSensori != oldLetturaSensori)
   {
@@ -126,9 +127,15 @@ void loop()
 
 void cicloMacchinaStati(unsigned long statoIngressi)
 {
-  statoPL1 = cicloMacchinaPL(statoPL1, SENS_PL(1, statoIngressi), &contaAssiPL1);
-  statoPL2 = cicloMacchinaPL(statoPL2, SENS_PL(2, statoIngressi), &contaAssiPL2);
-  statoPL3 = cicloMacchinaPL(statoPL3, SENS_PL(3, statoIngressi), &contaAssiPL3);
+  for (size_t i = 0; i < NUMERO_MAX_PL; i++)
+  {
+    passaggioLivello[i].statoPL =
+      cicloMacchinaPL(
+        passaggioLivello[i].statoPL,
+        SENS_PL(i, statoIngressi),
+        &(passaggioLivello[i].contaAssi)
+      );
+  }
 }
 
 byte cicloMacchinaPL(byte statoAttuale, byte ingressi, int *contaAssi)
@@ -236,81 +243,18 @@ unsigned long leggiSensoriPL()
 
 void movimentaPL()
 {
-  /* Gestione sbarre */
-  if (contaAssiPL1 > 0)
+  for (size_t i = 0; i < NUMERO_MAX_PL; i++)
   {
-    // Abbassa sbarra
-#ifdef SERVO360
-    if (statoSbarraPL1 == SBARRA_ALZATA)
-    {
-      servoPL1.write(70);
-      delay(300);
-      servoPL1.write(90);
-      statoSbarraPL1 = SBARRA_ABBASSATA;
-    }
-#else
-    servoPL1.write(180);
-#endif
-  }
-  else
-  {
-    /* Alza la sbarra */
-#ifdef SERVO360
-    if (statoSbarraPL1 == SBARRA_ABBASSATA)
-    {
-      servoPL1.write(110);
-      delay(290);
-      servoPL1.write(90);
-      statoSbarraPL1 = SBARRA_ALZATA;
-    }
-#else
-    servoPL1.write(90);
-#endif
-  }
-  /* Gestione semafori */
-  if ((SENS_PL(1, letturaSensori)) || (contaAssiPL1 > 0))
-    digitalWrite(SEMAFORO_PL1, HIGH);
-  else
-    digitalWrite(SEMAFORO_PL1, LOW);
-}
+    if (passaggioLivello[i].contaAssi > 0)
+      passaggioLivello[i].servoPL.write(180);
+    else
+      passaggioLivello[i].servoPL.write(90);
 
-unsigned long simulaIngressi(unsigned long ingressiAttuali)
-{
-  if (Serial.available() > 0)
-  {
-    // read the incoming byte:
-    int incomingPLnumber = Serial.parseInt();
-    if (incomingPLnumber > 0 && incomingPLnumber < 4)
-    {
-      Serial.print("PL selezionato: ");
-      Serial.println(incomingPLnumber, DEC);
-      Serial.println("Sequenza di input? (es.: [1000])");
-      String statoBit = Serial.readString();
-      Serial.print("implementa: ");
-      Serial.println(statoBit);
-      int provv = 0;
-      for (size_t i = 1; i < statoBit.length() - 1; i++)
-      {
-        provv = (provv << 1) | ((statoBit[i] == '1') ? 0x01 : 0);
-      }
-      provv <<= 4 * (incomingPLnumber - 1);
-      Serial.print("dato elaborato: ");
-      Serial.println(provv, BIN);
-      switch (incomingPLnumber)
-      {
-      case 1:
-        ingressiAttuali = ingressiAttuali & 0xFF0 | provv;
-        break;
-      case 2:
-        ingressiAttuali = ingressiAttuali & 0xF0F | provv;
-        break;
-      case 3:
-        ingressiAttuali = ingressiAttuali & 0x0FF | provv;
-        break;
-      }
-    }
+    if ((SENS_PL(i, letturaSensori)) || (passaggioLivello[i].contaAssi > 0))
+      digitalWrite(passaggioLivello[i].semaforo, HIGH);
+    else
+      digitalWrite(passaggioLivello[i].semaforo, LOW);
   }
-  return ingressiAttuali;
 }
 
 #define FRMT_BIN_8(I) ("000000000000" + String(I, BIN)).substring(String(I, BIN).length())
@@ -319,11 +263,12 @@ void visualizzaStato()
 {
   Serial.print("Stato ingressi ultima lettura: ");
   Serial.println(FRMT_BIN_8(letturaSensori));
-  Serial.print("Conta assi PL [1] [2] [3] ->  [");
-  Serial.print(contaAssiPL1);
-  Serial.print("]  [");
-  Serial.print(contaAssiPL2);
-  Serial.print("]  [");
-  Serial.print(contaAssiPL3);
-  Serial.println("]");
+  Serial.print("Conta assi PL [1] [2] [3] -> ");
+  for (size_t i = 0; i < NUMERO_MAX_PL; i++)
+  {
+    Serial.print(" [");
+    Serial.print(passaggioLivello[i].contaAssi);
+    Serial.print("]");
+  }
+  Serial.println();
 }
